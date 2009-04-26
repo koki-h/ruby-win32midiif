e1baee16627685abcfbe8d5574acc3a19a7bc6d3
U-koki-PC\koki <koki@koki-PC.(none)>
2009-04-26 23:27:15 +0900

    Bug fixed : When channelhas been opened many time, exception has been throwed.

--- winMidiIF.rb
+++ winMidiIF.rb
@@ -65,7 +65,7 @@ class MidiDevice
     @midiOutShortMsg.call(@ptr,msg)   # メッセージを送信
   end
   def openChannel(no)
-    channel = @using_channels.find {|c| c.no == no}
+    channel = @using_channels.find {|c| c == no}
     unless channel
       channel = MidiChannel.new(self,no)
       @using_channels.push(channel)
@@ -210,6 +210,56 @@ SOUND_LIST = {
   0x7E => "Applause",
   0x7F => "Gunshot"
 }
+
+RYTHM_NOTE_MAP = {
+  35 => "Acoustic Bass Drum",
+  36 => "Bass Drum1",
+  37 => "Side Stick",
+  38 => "Acoustic Snare",
+  39 => "Hand Clap",
+  40 => "Electric Snare",
+  41 => "Low Floor Tom",
+  42 => "Closed Hi-Hat",
+  43 => "High Floor Tom",
+  44 => "Pedal Hi-Hat",
+  45 => "Low Tom",
+  46 => "Open Hi-Hat",
+  47 => "Low-Mid Tom",
+  48 => "Hi-Mid Tom",
+  49 => "Crash Cymbal1",
+  50 => "High Tom",
+  51 => "Ride Cymbal1",
+  52 => "Chinese Cymbal",
+  53 => "Ride Bell",
+  54 => "Tambourine",
+  55 => "Splash Cymbal",
+  56 => "Cowbell",
+  57 => "Crash Cymbal2",
+  58 => "Vibraslap",
+  59 => "Ride Cymbal2",
+  60 => "Hi Bongo",
+  61 => "Low Bongo",
+  62 => "Mute Hi Conga",
+  63 => "Open Hi Conga",
+  64 => "Low Conga",
+  65 => "High Timbale",
+  66 => "Low Timbale",
+  67 => "High Agogo",
+  68 => "Low Agogo",
+  69 => "Cabasa",
+  70 => "Maracas",
+  71 => "Short Whistle",
+  72 => "Long Whistle",
+  73 => "Short Guiro",
+  74 => "Long Guiro",
+  75 => "Clavas",
+  76 => "Hi Wood Block",
+  77 => "Low Wood Block",
+  78 => "Mute Cuica",
+  79 => "Open Cuica",
+  80 => "Mute Triangle",
+  81 => "Open Triangle"
+}
 #テスト
 if __FILE__ == $0
   midi = MidiDevice.new
@@ -246,5 +296,6 @@ if __FILE__ == $0
     drum_cannel.playMidiSound(67,127)
     sleep(0.5) # 発音時間
   end
+  drum_cannel = midi.openChannel(9)
   midi.close
 end
